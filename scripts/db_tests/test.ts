6763528010d5c281d70c904602e1243c3f22841b
Lanadk <ottmanbecuwe@gmail.com>
2026-02-02 14:36:58 +0100

    prisma init

---
+++ scripts/db_tests/test.ts
@@ -0,0 +1,17 @@
+import {prisma} from "@/lib/prisma";
+
+
+async function main() {
+    const deputes = await prisma.depute.findMany()
+    console.log("Tous les députés :", deputes)
+
+    const jeandupont = await prisma.depute.findUnique({
+        where: { id: 1 },
+        include: { mandats: true },
+    })
+    console.log("Jean Dupont :", jeandupont)
+}
+
+main()
+    .catch(console.error)
+    .finally(async () => await prisma.$disconnect())
