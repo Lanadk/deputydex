989e6fa5ad3581c858d4bcb658dd9b95d76931bb
Lanadk <ottmanbecuwe@gmail.com>
2026-03-06 18:52:14 +0100

    fix

--- app/component-library/molecules/deputy-card/deputey-card.constants.tsx
+++ app/component-library/molecules/deputy-card/deputey-card.constants.tsx
@@ -18,27 +18,27 @@ export const getDeputyCardSections = () => [
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 items-start">
                 <DeputyCard nom="Thivauetjr ehjrkedjdje"
                             groupe="LFI"
-                            image="/tribun/16/photos_deputes_no_bg/001_Damien_Abad.png"
+                            image="/tribun/16/photos_deputes_nobg/001_Damien_Abad.png"
                             role="Député"
                 />
                 <DeputyCard nom="Titi"
                             groupe="RN"
-                            image="/tribun/16/photos_deputes_no_bg/026_Anne-Laure_Babault.png"
+                            image="/tribun/16/photos_deputes_nobg/026_Anne-Laure_Babault.png"
                             role="Député"
                 />
                 <DeputyCard nom="Tata Micron"
                             groupe="RE"
-                            image="/tribun/16/photos_deputes_no_bg/010_Henri_Alfandari.png"
+                            image="/tribun/16/photos_deputes_nobg/010_Henri_Alfandari.png"
                             role="Président"
                 />
                 <DeputyCard nom="Toto Micron"
                             groupe="SOC"
-                            image="/tribun/16/photos_deputes_no_bg/032_Christophe_Barthès.png"
+                            image="/tribun/16/photos_deputes_nobg/032_Christophe_Barthès.png"
                             role="Gouvernement"
                 />
                 <DeputyCard nom="toto"
                             groupe="ECOS"
-                            image="/tribun/16/photos_deputes_no_bg/111_Agnès_Carel.png"
+                            image="/tribun/16/photos_deputes_nobg/111_Agnès_Carel.png"
                             role="Ministre"
                 />
             </div>
