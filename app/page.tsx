893e2e8b51d2977ac1531733e3622fdb7a545bd5
Lanadk <ottmanbecuwe@gmail.com>
2026-03-09 00:45:42 +0100

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+import {ButtonLib} from "@/app/(ui)/component-library/atoms/button/button-lib";
+
 export default function Home() {
-  return (
-    <div>
-        <main>
-            <p>Toto</p>
-        </main>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen gap-8">
+            <h1>Bienvenue</h1>
+            <div className="flex gap-4">
+                <Link href="/deputydex">
+                    <ButtonLib text="DeputeDex" variant="primary" size="large" />
+                </Link>
+                <Link href="/globaldb/deputies">
+                    <ButtonLib text="Global DB" variant="secondary" size="large" />
+                </Link>
+            </div>
+        </div>
+    );
 }
