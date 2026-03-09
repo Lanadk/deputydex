a5bd938f2ed1b9e57efbf3f7e3d20465e414829b
Lanadk <ottmanbecuwe@gmail.com>
2026-03-09 01:50:15 +0100

    route updated

--- app/(ui)/components/sidebar/nave-items.ts
+++ app/(ui)/components/sidebar/nave-items.ts
@@ -6,8 +6,8 @@ export const NAVITEMS = [
     {label: "Groupes", href: "/db/groups", icon: Layers, section: "db"},
     {label: "Votes", href: "/db/votes", icon: Vote, section: "db"},
     {label: "Statistiques", href: "/db/statistics", icon: BarChart3, section: "db"},
-    {label: "Data Sources", href: "/info/data-sources", icon: Database, section: "info"},
-    {label: "About us", href: "/info/about-us", icon: MessageCircleMore, section: "info"},
+    {label: "Data Sources", href: "/data-sources", icon: Database, section: "info"},
+    {label: "About us", href: "/about-us", icon: MessageCircleMore, section: "info"},
     {label: "Donations", href: "/donations", icon: HandHelping, section: "info"},
     {label: "Lib component", href: "/component-library", icon: Component, section: "component-library"}, // dev only , faudra couper la route en prod
 ];
