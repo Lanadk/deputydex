e547f9c56f76cecd753791bc31c9b0874875d303
Lanadk <ottmanbecuwe@gmail.com>
2026-03-27 17:34:44 +0100

    deputedex logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
+"use client";
+
 import Link from "next/link";
-import {ButtonLib} from "@/app/(ui)/component-library/atoms/button/button-lib";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { ButtonLib } from "@/app/(ui)/component-library/atoms/button/button-lib";
 
 export default function Home() {
+    const { resolvedTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setMounted(true);
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen gap-8">
+            <div className="h-35 flex items-center justify-center">
+                {mounted && (
+                    <Image
+                        src={
+                            resolvedTheme === "light"
+                                ? "/assets/imgs/ASS_DEX_LIGHT.svg"
+                                : "/assets/imgs/ASS_DEX_ACC.svg"
+                        }
+                        alt="ASS DEX"
+                        width={140}
+                        height={140}
+                        priority
+                    />
+                )}
+            </div>
+
             <h1>Bienvenue</h1>
+
             <div className="flex gap-4">
                 <Link href="/deputydex">
                     <ButtonLib text="DeputeDex" variant="primary" size="large" />
