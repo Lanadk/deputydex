1d033ef627d809786a9de0250bd7a18f6f351bbb
Lanadk <ottmanbecuwe@gmail.com>
2026-03-30 18:30:29 +0200

    fix folder

--- app/(ui)/(views)/(db)/groupes/[code]/registry.ts
+++ app/(ui)/(views)/(db)/groupes/[code]/registry.ts
@@ -1,6 +1,6 @@
 import {GROUPES_KPI_REGISTRY} from "@/app/(ui)/(views)/(db)/groupes/[code]/groupes-kpi-card.registry";
 import {GROUPES_ACTIVITY_CALENDAR} from "@/app/(ui)/(views)/(db)/groupes/[code]/groupes-activity-calendar.registry";
-import {makeRegistryHelper} from "@/app/_shared/registry/registry.helper";
+import {makeRegistryHelper} from "@/app/(ui)/_shared/registry/registry.helper";
 import {GROUPE_TABLE_REGISTRY} from "@/app/(ui)/(views)/(db)/groupes/[code]/groupes-table.registry";
 
 export const card = makeRegistryHelper(GROUPES_KPI_REGISTRY, "CardConfig");
