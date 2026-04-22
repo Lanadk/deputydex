e105ff670e54468138029229e3b557dc4e5393c6
F5OTT <ottmanbecuwe@gmail.com>
2026-04-22 13:12:55 +0200

    correcting request

--- app/infrastructure/groupes/repositories/prisma-groupe-members.repository.ts
+++ app/infrastructure/groupes/repositories/prisma-groupe-members.repository.ts
@@ -14,32 +14,35 @@ export const prismaGroupeMembersRepository: IGroupeMembersRepository = {
     async getGroupeMembers(code: string, legislature: number): Promise<GroupeMembersRow[]> {
         try {
             return await prisma.$queryRaw<GroupeMembersRow[]>`
-                SELECT a.prenom                                                    AS first_name,
-                       a.nom                                                       AS last_name,
-                       ag.date_debut                                               AS since,
-                       DATE_PART('year', AGE(CURRENT_DATE, a.date_naissance))::int AS age,
-                    CONCAT(m.election_departement, ' - ', m.election_num_circo, 'e circ.') AS circonscription
-                FROM acteurs_groupes ag
-                         INNER JOIN acteurs a
-                                    ON a.uid = ag.acteur_uid
-                         INNER JOIN ref_groupes rg
-                                    ON rg.groupe_id = ag.groupe_id
-                                        AND rg.groupe_legislature = ag.groupe_legislature
-                         LEFT JOIN LATERAL (
-                            SELECT mv.election_departement,
-                                   mv.election_num_circo
-                            FROM mandats mv
-                            WHERE mv.acteur_uid = a.uid
-                              AND mv.legislature = ag.groupe_legislature
-                              AND mv.type_organe = 'ASSEMBLEE'
-                            ORDER BY mv.date_debut DESC
-                                LIMIT 1
-                         ) m ON true
-                WHERE rg.code = ${code}
-                  AND ag.groupe_legislature = ${legislature}
-                  AND ag.date_debut <= CURRENT_DATE
-                  AND (ag.date_fin IS NULL OR ag.date_fin >= CURRENT_DATE)
-                ORDER BY a.nom, a.prenom
+                SELECT * FROM (
+                                  SELECT DISTINCT ON (a.uid)
+                                      a.prenom                                                    AS first_name,
+                                      a.nom                                                       AS last_name,
+                                      ag.date_debut                                               AS since,
+                                      DATE_PART('year', AGE(CURRENT_DATE, a.date_naissance))::int AS age,
+                                      CONCAT(m.election_departement, ' - ', m.election_num_circo, 'e circ.') AS circonscription
+                                  FROM acteurs_groupes ag
+                                      INNER JOIN acteurs a ON a.uid = ag.acteur_uid
+                                      INNER JOIN ref_groupes rg
+                                      ON rg.groupe_id = ag.groupe_id
+                                      AND rg.groupe_legislature = ag.groupe_legislature
+                                      LEFT JOIN LATERAL (
+                                      SELECT mv.election_departement, mv.election_num_circo
+                                      FROM mandats mv
+                                      WHERE mv.acteur_uid = a.uid
+                                      AND mv.legislature = ag.groupe_legislature
+                                      AND mv.type_organe = 'ASSEMBLEE'
+                                      ORDER BY mv.date_debut DESC
+                                      LIMIT 1
+                                      ) m ON true
+                                      LEFT JOIN ref_acteurs_photos p ON p.acteur_uid = a.uid
+                                  WHERE rg.code = ${code}
+                                    AND ag.groupe_legislature = ${legislature}
+                                    AND ag.date_debut <= CURRENT_DATE
+                                    AND (ag.date_fin IS NULL OR ag.date_fin >= CURRENT_DATE)
+                                  ORDER BY a.uid, ag.date_fin DESC NULLS FIRST, ag.date_debut DESC
+                              ) sub
+                ORDER BY last_name, first_name
             `;
 
         } catch (error) {
